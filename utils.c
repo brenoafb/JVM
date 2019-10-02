2c684c40160667e29c3cf1649d8a2425d19a088e
brenoafb <breno.afb@gmail.com>
2019-10-01 22:50:55 -0300

    Change endian functions.

--- utils.c
+++ utils.c
@@ -1,17 +1,12 @@
 #include "utils.h"
 
 uint16_t switch_endian_16(uint16_t x) {
-  uint16_t tmp = x;
-  tmp >>= 8;
-  x <<= 8;
-  return x + tmp;
+  return (x >> 8) | ((x << 8) & 0xff);
 }
 
 uint16_t switch_endian_32(uint32_t x) {
-  uint16_t lo = x;
-  uint16_t hi = (x >> 8);
-  lo = switch_endian_16(lo);
-  hi = switch_endian_16(hi);
-
-  return (hi << 8) + lo;
+  return ((x >> 24)& 0xff)      |
+	 ((x << 8) & 0xff0000)  |
+	 ((x >> 8) & 0xff00)    |
+	 ((x << 24)& 0xff000000);
 }
