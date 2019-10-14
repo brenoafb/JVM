cba7758f4ca07506093c8a473a49ca70a0ffe15d
brenoafb <breno.afb@gmail.com>
2019-10-14 09:04:55 -0300

    Add classfile docs.

--- classfile.h
+++ classfile.h
@@ -6,6 +6,9 @@
 #include "utils.h"
 #include "types.h"
 
+/**
+ * Represents the class file.
+ */
 typedef struct {
   uint32_t magic;
 
@@ -33,44 +36,166 @@ typedef struct {
   attribute_info *attributes; /*n=attributes_count */
 } classfile;
 
+/**
+ * Reads class file open in fp.
+ * @param cf pointer to struct to which the data will be read to.
+ * @param fp file pointer to open class file in read mode.
+ */
 void read_class_file(classfile *cf, FILE *fp);
 
+/**
+ * Reads the class file's constant pool.
+ * @param fp file pointer situated at the first byte of the constant pool.
+ * @param cp array to where the pool will be read.
+ * @param cpsize size of the constant pool (as read from the class file).
+ */
 void read_constant_pool(FILE *fp, cp_info cp[], int cpsize);
+
+/**
+ * Read a single entry from the constant pool.
+ */
 void read_constant_pool_entry(FILE *fp, cp_info *cp);
 
+/**
+ * Read the fields table of the class file.
+ */
 void read_fields(FILE *fp, field_info fields[], uint16_t fields_count, cp_info *cp);
+
+/**
+ * Read a single field entry from the fields table.
+ */
 void read_field_entry(FILE *fp, field_info *field, cp_info *cp);
 
+/**
+ * Read the methods table of the class file.
+ */
 void read_methods(FILE *fp, method_info methods[], uint16_t methods_count, cp_info *cp);
+
+/**
+ * Read a single method entry from the methods table.
+ */
 void read_method_entry(FILE *fp, method_info *method, cp_info *cp);
 
+/**
+ * Read class file attributes table.
+ */
 void read_attributes(FILE *fp, attribute_info attributes[], uint16_t attributes_count, cp_info *cp);
+
+/**
+ * Read a single entry from some attributes table.
+ */
 void read_attribute_info(FILE *fp, attribute_info *ptr, cp_info *cp);
 
+/**
+ * Read Code attribute.
+ */
 void read_code_attribute(Code_attribute *ptr, FILE *fp, cp_info *cp);
+
+/**
+ * Read ConstantValue attribute.
+ */
 void read_constantvalue_attribute(ConstantValue_attribute *ptr, FILE *fp);
+
+/**
+ * Read Exceptions attribute.
+ */
 void read_exceptions_attribute(Exceptions_attribute *ptr, FILE *fp);
+
+/**
+ * Read LineNumberTable attribute.
+ */
 void read_linenumbertable_attribute(LineNumberTable_attribute *ptr, FILE *fp);
+
+/**
+ * Read SourceFile attribute.
+ */
 void read_sourcefile_attribute(SourceFile_attribute *ptr, FILE *fp);
 
+/**
+ * Return pointer to string in constant pool table.
+ * Entry in constant pool must have tag CONSTANT_Utf8.
+ * Indices start from 0.
+ */
 char *get_cp_string(cp_info *cp, uint16_t index);
 
+/**
+ * Print class file summary.
+ * i.e. Prints the values read but not the tables.
+ */
 void print_class_file_summary(classfile *cf);
+
+/**
+ * Print constant pool detail.
+ * Prints the type of each entry.
+ * If an entry has type Utf8, the corresponding string is printed.
+ */
 void print_cp_detail(classfile *cf);
 
+/**
+ * Deinitializes a single constant pool entry.
+ */
 void deinit_cp_entry(cp_info *ptr);
+
+/**
+ * Deinitializes the constant pool.
+ */
 void deinit_constant_pool(cp_info cp[], uint16_t cpsize);
+
+/**
+ * Denitializes the class file struct.
+ */
 void deinit_class_file(classfile *cf);
 
+/**
+ * Deinitializes fields table.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_fields(field_info fields[], uint16_t fields_count, cp_info *cp);
+
+/**
+ * Deinitializes single field entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_field_entry(field_info *ptr, cp_info *cp);
 
+/**
+ * Deinitializes methods table.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_methods(method_info methods[], uint16_t method_count, cp_info *cp);
+
+/**
+ * Deinitializes single method entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_method_entry(method_info *ptr, cp_info *cp);
 
+/**
+ * Deinitializes attributes table
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_attributes(attribute_info attributes[], uint16_t attributes_count, cp_info *cp);
+
+/**
+ * Deinitializes single attribute entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_attribute_info(attribute_info *ptr, cp_info *cp);
 
+/**
+ * Deinitializes Code attribute entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_code_attribute(Code_attribute *ptr, cp_info *cp);
+
+/**
+ * Deinitializes Exceptions attribute entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_exceptions_attribute(Exceptions_attribute *ptr);
+
+/**
+ * Deinitializes LineNumberTable attribute entry.
+ * @param cp points to a valid constant pool (i.e. read, not deinitialized)
+ */
 void deinit_linenumbertable_attribute(LineNumberTable_attribute *ptr);
