5a08672eae61fae911e1ad60547df9d22a049065
brenoafb <breno.afb@gmail.com>
2019-10-27 15:13:31 -0300

    Add jvm, frame, methodarea, opcodes to makefile.

--- frame.h
+++ frame.h
@@ -6,12 +6,12 @@
 #include "classfile.h"
 
 typedef struct Frame {
-  uint32_t n_locals;      // computed at compile time
-  uint32_t *locals;       // array of local variables
-  uint32_t n_operands;    // computed at compile time
-  uint8_t *operands;      // stack of operands
-  uint32_t i;             // top of stack index
-  cp_info *cp;            // reference to constant pool of class
+  uint32_t n_locals;      /* computed at compile time             */
+  uint32_t *locals;       /* array of local variables             */
+  uint32_t n_operands;    /* computed at compile time             */
+  uint8_t *operands;      /* stack of operands                    */
+  uint32_t i;             /* top of stack index                   */
+  cp_info *cp;            /* reference to constant pool of class  */
 } Frame;
 
 void init_frame(Frame *f, uint32_t n_locals, uint32_t n_operands, cp_info *cp);
