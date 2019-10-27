217479b6c6a9be56d590f38e9e7f682e863a11a6
brenoafb <breno.afb@gmail.com>
2019-10-27 15:59:47 -0300

    Add initial method definitions.

--- jvm.c
+++ jvm.c
@@ -1,1 +1,20 @@
 #include "jvm.h"
+
+void init_jvm(JVM *jvm) {
+  jvm->pc = 0;
+  jvm->frame_index = -1; /* no frames initially */
+  jvm->method_area = calloc(sizeof(MethodArea), 1);
+  init_method_area(jvm->method_area);
+  jvm->current_class_index = -1;
+  jvm->current_method_index = -1;
+}
+
+void deinit_jvm(JVM *jvm) {
+  deinit_method_area(jvm->method_area);
+  free(jvm->method_area);
+}
+
+/* Load the classfile into the jvm's MethodArea */
+void jvm_load_class(JVM *jvm, classfile *cf) {
+  method_area_load_class(jvm->method_area, cf);
+}
--- jvm.h
+++ jvm.h
@@ -6,20 +6,23 @@
 #include "classfile.h"
 #include "frame.h"
 #include "methodarea.h"
+#include "opcodes.h"
+
+#define MAX_FRAMES 32
 
 typedef struct JVM {
   uint32_t pc;
-  Frame *frames;
-  uint32_t fi;    /* top of frame stack index */
+  Frame *frames[MAX_FRAMES];
+  int32_t frame_index;    /* top of frame stack index */
   /* uint8_t *heap; */
   MethodArea *method_area;
   /* NativeMethodArea nma; */
-  uint32_t current_class_index;
-  uint32_t current_method_index;
+  int32_t current_class_index;
+  int32_t current_method_index;
 } JVM;
 
 void init_jvm(JVM *jvm);
 void deinit_jvm(JVM *jvm);
 
 /* Load the classfile into the jvm's MethodArea */
-void load_class(JVM *jvm, classfile *cf);
+void jvm_load_class(JVM *jvm, classfile *cf);
