f35b7e6eb9273c7b2048f9978b1376aa619ecd10
FilipiT <filipi_teles@hotmail.com>
2019-11-23 01:31:04 -0300

    Add files via upload

---
+++ samples/Add_Doubles.java
@@ -0,0 +1,14 @@
+class AddDoubles {
+
+  public static void main (String args[]) {
+
+    double x = 7.5;
+    double y = 5.4; 
+
+    double z = x + y;
+    System.out.println("x + y is " + z);
+
+
+  }
+
+ } 
