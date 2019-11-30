0ca18c93815eb168c47c8b4172db8858794cc37e
brenoafb <breno.afb@gmail.com>
2019-11-30 00:11:20 -0300

    Add simple function handling code.

---
+++ samples/SimpleFunction.java
@@ -0,0 +1,11 @@
+public class SimpleFunction {
+    public static void main(String []args) {
+	    System.out.println("Before function.");
+	    func();
+	    System.out.println("After function.");
+    }
+
+    public static void func() {
+	    System.out.println("Inside function.");
+    }
+}
