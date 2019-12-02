f3965b731ade5f1bd316cb92cc7a64064df3f4a1
brenoafb <breno.afb@gmail.com>
2019-12-02 16:19:11 -0300

    Add double cast example.

---
+++ samples/DoubleCast.java
@@ -0,0 +1,10 @@
+public class DoubleCast {
+    public static void main (String args[]) {
+	System.out.println("xInt:");
+	int xInt = 1;
+	System.out.println(xInt);
+	System.out.println("xDouble:");
+	double xDouble = xInt;
+	System.out.println(xDouble);
+    }
+}
