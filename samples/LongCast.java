08a0d855f2dce072ae5dbae321f75626cbff98a9
brenoafb <breno.afb@gmail.com>
2019-12-02 16:49:03 -0300

    Add LongCast example.

---
+++ samples/LongCast.java
@@ -0,0 +1,10 @@
+public class LongCast {
+    public static void main (String args[]) {
+	System.out.println("xInt:");
+	int xInt = 1;
+	System.out.println(xInt);
+	System.out.println("xLong:");
+	long xLong = xInt;
+	System.out.println(xLong);
+    }
+}
