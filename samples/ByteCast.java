1f1c455f305a0ca3920ebc7519156db7414af39d
brenoafb <breno.afb@gmail.com>
2019-12-02 16:53:09 -0300

    Add byte cast example.

---
+++ samples/ByteCast.java
@@ -0,0 +1,10 @@
+public class ByteCast {
+    public static void main (String args[]) {
+	System.out.println("xInt:");
+	int xInt = 1;
+	System.out.println(xInt);
+	System.out.println("xByte:");
+	byte xByte = (byte) xInt;
+	System.out.println(xByte);
+    }
+}
