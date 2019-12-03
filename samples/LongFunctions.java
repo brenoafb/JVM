1e3de14b36421e90586cef7bd17e6c33bd675159
brenoafb <breno.afb@gmail.com>
2019-12-03 15:18:48 -0300

    Add long function example.

--- samples/LongFunctions.java
+++ samples/LongFunctions.java
@@ -4,9 +4,18 @@ public class LongFunctions {
 	return -9223372036854775808l;
     }
 
+    public static long longIdentity(long x) {
+	return x;
+    }
+
     public static void main(String[] args) {
 	long smallLong = getSmallLong();
 	System.out.println(smallLong);
+
+	long bigLong = 922337203685477580l;
+	System.out.println(bigLong);
+	bigLong = longIdentity(bigLong);
+	System.out.println(bigLong);
 
     }
 }
