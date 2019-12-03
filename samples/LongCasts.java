15836f55cfdf883157e3441703c5a1a5532362a4
brenoafb <breno.afb@gmail.com>
2019-12-02 21:44:04 -0300

    Add conversion samples.

--- samples/LongCasts.java
+++ samples/LongCasts.java
@@ -1,18 +1,18 @@
-public class DoubleCasts {
+public class LongCasts {
 	public static void main(String args[]) {
-		double d = 3.14159;
-		float f = (float) d;
-		int i = (int) d;
-		long l = (long) d;
+		long l = 314159;
+		double d = (double) l;
+		float f = (float) l;
+		int i = (int) l;
 
+		System.out.println("long:");
+		System.out.println(l);
 		System.out.println("double:");
 		System.out.println(d);
 		System.out.println("float:");
 		System.out.println(f);
 		System.out.println("int:");
 		System.out.println(i);
-		System.out.println("long:");
-		System.out.println(l);
 
 	}
 }
